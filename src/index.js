65392095fbf0832d0769a40b0339c3021c1c71ab
techp-akashab <akash.bheke@techprescient.com>
2025-03-06 15:10:39 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import DisplayTodos from "./components/Todos/displayTodos.js";
 import About from "./components/About/About.js";
+import './index.css'
 const root = document.getElementById("root");
 document.getElementById("home").addEventListener("click", DisplayTodos);
 document.getElementById("about").addEventListener("click", About);
